6185df7b06140407a506c0ea20d149295d94ff0d
Bobby <sgmaniac1255@yahoo.com>
2017-12-07 20:13:03 -0600

    blah

--- src/chess/board/Board.java
+++ src/chess/board/Board.java
@@ -2,6 +2,13 @@ package chess.board;
 
 public class Board {
 
-	Square board[][] = new Square[8][8];
+	private Square board[][] = new Square[8][8];
 	
+	private void popBoard(){
+		for (int i = 0; i < board.length; i++) {
+			for (int j = 0; j < board[i].length; j++) {
+				board[i][j]=new Square(i,j);
+			}
+		}
+	}
 }
--- src/chess/board/Square.java
+++ src/chess/board/Square.java
@@ -2,12 +2,12 @@ package chess.board;
 
 public class Square {
 	
-	public final Rank r;
-	public final File f;
-	
-	public Square(Rank r, File f){
-		this.r=r;
-		this.f=f;
+	public final int RANK;
+	public final int FILE;
+
+	public Square(int rank, int file) {
+		this.RANK=rank;
+		this.FILE=file;
 	}
 
 }
